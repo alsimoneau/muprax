2d6f72bcf031aa7f2aa3b23490b768f394dadb46
Alexandre Simoneau <alsimoneau@gmail.com>
2022-08-29 11:25:24 -0400

    Debug

--- muprax.py
+++ muprax.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
 
-import click
-import joblib
 import numpy as np
 import pandas as pd
 import pyproj
 import rasterio
 import rasterio.warp
 import scipy.ndimage
-
-
-def parallelize(func):
-    def wrapper(iterable, *args):
-        return joblib.Parallel(n_jobs=-1, prefer="threads")(
-            joblib.delayed(func)(i, *args) for i in iterable
-        )
-
-    return wrapper
+import yaml
 
 
 def transform(s_crs=4326, t_crs=4326):
@@ -27,12 +17,13 @@ def estimate_utm_epsg(lon, lat):
     return pyproj.database.query_utm_crs_info(
         datum_name="WGS84",
         area_of_interest=pyproj.aoi.AreaOfInterest(
-            min(lon), min(lat), max(lon), max(lat)
+            np.min(lon), np.min(lat), np.max(lon), np.max(lat)
         ),
     )[0].code
 
 
-def warp(src, crs, resampling):
+def warp(src, epsg, resampling):
+    crs = rasterio.CRS.from_epsg(epsg)
     T, w, h = rasterio.warp.calculate_default_transform(
         src.crs, crs, src.width, src.height, *src.bounds
     )
@@ -49,15 +40,19 @@ def warp(src, crs, resampling):
     )
 
 
+def arr_equal(a, b):
+    return len(a) == len(b) and np.all(a == b)
+
+
 def main(points, raster, func, params):
     db = pd.read_csv(points)
-    if db.columns != ["ID", "Latitude", "Longitude"]:
+    if not arr_equal(db.columns, ["ID", "Latitude", "Longitude"]):
         raise ValueError("Wrong point list format")
 
     lat = db["Latitude"]
     lon = db["Longitude"]
 
-    rst = rio.open(raster)
+    rst = rasterio.open(raster)
 
     epsg = estimate_utm_epsg(lon.mean(), lat.mean())
     y, x = transform(4326, epsg)(lat, lon)
@@ -81,7 +76,16 @@ def main(points, raster, func, params):
         arr = scipy.ndimage.convolve(arr, kernel, modemode="constant")
 
     db["Interp"] = arr[rasterio.transform.rowcol(T_warp, y, x)]
+    db.to_csv("out.csv")
 
 
 if __name__ == "__main__":
-    main()
+    with open("muprax.in") as f:
+        p = yaml.safe_load(f)
+
+    main(
+        points=p["points_file"],
+        raster=p["raster_file"],
+        func=p["mode"],
+        params=p["params"],
+    )
